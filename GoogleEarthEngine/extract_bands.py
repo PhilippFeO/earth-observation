4778580acab11b496f65dc914bbe2a3e0408ff09
Philipp Rost <rost.philipp@protonmail.com>
2023-08-12 21:25:40 +0200

    Code structured; CLI arguments added

--- GoogleEarthEngine/extract_bands.py
+++ GoogleEarthEngine/extract_bands.py
@@ -1,69 +1,73 @@
 import rasterio
 import numpy as np
+import argparse
 
 
-geotiff = rasterio.open('./geotiffs/all_bands_800.geotiff')
+def adjust_values(data):
+    ''' Linear Mapping
+    In a nutshell: Map values from 0-65536 to 0-255 '''
+    # All bands containing geotiff image contains values in 0-65536 (to be precise, [u]int16).
+    # But the range is just partially exploited, ie. only values from f.e. 5000-20000.
+    # To avoid dark images, the effective range is mapped onto 0-255.
+    # Due to technical reasons (s. link below) the range should be truncated to
+    # achive higher quality images
+    #   https://gis.stackexchange.com/questions/304180/what-are-the-min-and-max-values-of-map-addlayer-on-google-earth-engine
+    # The original template (s. NDVI-Munich.ipynb) removes values <7000 and >16000, ie.
+    # maps the range 7000-16000 onto 0-255 (by specifying a <min> and <max> argument in a
+    # <parameters>-dictionary). This has to be replicated and it's done by solving
+    #   7000m + t = 0,
+    #   16000m + t = 255
+    #       => m = 255/9000, t = -198
 
-# Set geotiff specific meta data
-output_meta = geotiff.meta.copy()
-output_meta.update(count=1,  # number of channels
-                   dtype=rasterio.uint8,  # interpret values within 0-255
-                   nodata=0)  # placeholder if no data is avialable, fi. at the edges
-# print(output_meta)
-
-'''Save single RGB channels '''
-# image contains values in the range 0-65536
-# mapping them onto 0-255
-
-'''Remap image values'''
-# for band, name in zip((2, 3, 4), ("blue", "green", "red")):
-#     data = geotiff.read(band)
-#     d[band-2] = data
-#     # Linear mapping
-#     # all bands containing geotiff image contains values in 0-65536 (to be precise, [u]int16)
-#     # Due to technical reasons (s. link below) the range should be truncated to
-#     # achive higher quality images
-#     #   https://gis.stackexchange.com/questions/304180/what-are-the-min-and-max-values-of-map-addlayer-on-google-earth-engine
-#     # The original template (s. NDVI-Munich.ipynb) removes values <7000 and >16000, ie.
-#     # maps the range 7000-16000 onto 0-255 (by specifying a <min> and <max> argument in a
-#     # <parameters>-dictionary). This has to be replicated and it's done by solving
-#     #   7000m + t = 0,
-#     #   16000m + t = 255
-#     #       => m = 255/9000, t = -198
-#
-#     # Leave <nodata>-values untouched
-#     data = np.where(data == 0, 0, data * 255/9000 - 198)
-#     # Convert values to uint8 according to metadata
-#     data = data.astype('uint8')
-#
-#     with rasterio.open(f'./geotiffs/B{band}-{name}-extracted.geotiff', 'w', **output_meta) as sc:
-#         sc.write(data, 1)
+    # Leave <nodata>-values untouched
+    return np.where(data == 0, 0, data * 255/9000 - 198)
 
 
-output_meta.update(count=3)
-data = geotiff.read([4, 3, 2])
+def extract_band(geotiff, band, name):
+    data = geotiff.read(band)
+    # Leave <nodata>-values untouched
+    data = adjust_values(data)
+    # Convert values to uint8 according to metadata
+    data = data.astype('uint8')
 
-''' Linear Mapping
-# In a nutshell: Map values from 0-65536 to 0-255 '''
-# All bands containing geotiff image contains values in 0-65536 (to be precise, [u]int16)
-# Due to technical reasons (s. link below) the range should be truncated to
-# achive higher quality images
-#   https://gis.stackexchange.com/questions/304180/what-are-the-min-and-max-values-of-map-addlayer-on-google-earth-engine
-# The original template (s. NDVI-Munich.ipynb) removes values <7000 and >16000, ie.
-# maps the range 7000-16000 onto 0-255 (by specifying a <min> and <max> argument in a
-# <parameters>-dictionary). This has to be replicated and it's done by solving
-#   7000m + t = 0,
-#   16000m + t = 255
-#       => m = 255/9000, t = -198
+    output_meta = geotiff.meta.copy()
+    output_meta.update(count=1,  # number of channels
+                       dtype=rasterio.uint8,  # interpret values within 0-255
+                       nodata=0)  # placeholder if no data is avialable, fi. at the edges
 
-# Leave <nodata>-values untouched
-data = np.where(data == 0, 0, data * 255/9000 - 198)
+    with rasterio.open(f'./geotiffs/B{band}-{name}.geotiff', 'w', **output_meta) as sc:
+        sc.write(data, 1)
 
-# Save RGB image
-with rasterio.open('./geotiffs/rgb.tif', 'w', **output_meta) as m:
-    m.write(data)
 
-geotiff.close()
+def compose_bands_to_rgb(geotiff):
+    output_meta = geotiff.meta.copy()
+    output_meta.update(count=3,
+                       dtype=rasterio.uint8,  # interpret values within 0-255
+                       nodata=0)  # placeholder if no data is avialable, fi. at the edges
+    data = geotiff.read([4, 3, 2])
+
+    data = adjust_values(data)
+
+    # Save RGB image
+    with rasterio.open('./geotiffs/rgb.tif', 'w', **output_meta) as m:
+        m.write(data)
+
+
+if __name__ == "__main__":
+    geotiff = rasterio.open('./geotiffs/all_bands_800.geotiff')
+
+    # compose_bands_to_rgb()
+
+    parser = argparse.ArgumentParser("extract_bands")
+    parser.add_argument(
+        "band", help="The band to extract from a geotiff image", type=int)
+    parser.add_argument(
+        "band_name", help="Name of band, f.i. <red> or <near-IR>", type=str)
+    args = parser.parse_args()
+
+    extract_band(geotiff, args.band, args.band_name)
+
+    geotiff.close()
 
 
 # Some reference:
