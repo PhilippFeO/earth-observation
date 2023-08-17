878d29670519cbd88fbfe16061c0132bd5f1ad61
Philipp Rost <rost.philipp@protonmail.com>
2023-08-17 12:42:55 +0200

    Align bands correctly for RGB image

--- NDVI/combine_bands.py
+++ NDVI/combine_bands.py
@@ -1,3 +1,6 @@
+"""
+Reads image of band {red, green, blue} in <args.band_dir> (i.e., all images have to be transfered there beforehand) and combines them to an RGB image. This saved under '<args.band_dir>/out/combined_bands.tif'.
+"""
 import os
 import rasterio
 from extract_bands import adjust_values
@@ -16,21 +19,28 @@ args = p.parse_args()
 directory = os.fsencode(args.band_dir)
 files = os.listdir(directory)
 out_meta = None
-nmb_bands = len(files)
 bands = None
-for i, file in enumerate(files):
+for i, f in enumerate(files):
     # decode b-string to normal string
-    band_file = os.fsdecode(f'{args.band_dir}/{file.decode("utf-8")}')
+    f = f.decode('utf-8')
+    band_file = os.fsdecode(f'{args.band_dir}/{f}')
     if band_file.endswith(('.tif', '.TIF')):
         band = rasterio.open(band_file)
         if bands is None:
             out_meta = band.meta.copy()
-            bands = np.empty(
-                (nmb_bands, out_meta['height'], out_meta['width']))
-        bands[i] = band.read(1)
+            bands = np.empty((3,
+                              out_meta['height'],
+                              out_meta['width']))
+        # Place band at right position for RGB image
+        if 'B2' in f:  # 'B2' is blue
+            bands[2] = band.read(1)
+        elif 'B3' in f:  # 'B3' is green
+            bands[1] = band.read(1)
+        else:  # Remaining is 'B4', which is red
+            bands[0] = band.read(1)
         band.close()
 
-bands = adjust_values(bands, 5000, 10000)
+bands = adjust_values(bands, 7000, 16000)
 
 # Create directory for output image
 out_dir = f'{args.band_dir}/out'
@@ -39,7 +49,7 @@ try:
 except FileExistsError:
     print('out-directory exists')
 
-out_meta.update(count=nmb_bands,
+out_meta.update(count=3,
                 dtype=rasterio.uint8,
                 nodata=0)
 print(f'{out_meta = }')
