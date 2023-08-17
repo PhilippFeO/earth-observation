9f5fe8456864e56b906fb1e442511c79f5188206
Philipp Rost <rost.philipp@protonmail.com>
2023-08-17 12:02:28 +0200

    Renamed variable

--- NDVI/combine_bands.py
+++ NDVI/combine_bands.py
@@ -7,34 +7,33 @@ import numpy as np
 # TODO: adjust band values individually to control the influence of a band <17-08-2023>
 
 p = argparse.ArgumentParser("compose_bands")
-p.add_argument("image_dir",
+p.add_argument("band_dir",
                help="Directory containing the images for composing.",
                type=str)
 args = p.parse_args()
 
 
-directory = os.fsencode(args.image_dir)
+directory = os.fsencode(args.band_dir)
 files = os.listdir(directory)
 out_meta = None
 nmb_bands = len(files)
 bands = None
 for i, file in enumerate(files):
     # decode b-string to normal string
-    band_tif = os.fsdecode(f'{args.image_dir}/{file.decode("utf-8")}')
-    if band_tif.endswith(('.tif', '.TIF')):
-        band_tif = rasterio.open(band_tif)
+    band_file = os.fsdecode(f'{args.band_dir}/{file.decode("utf-8")}')
+    if band_file.endswith(('.tif', '.TIF')):
+        band = rasterio.open(band_file)
         if bands is None:
-            out_meta = band_tif.meta.copy()
+            out_meta = band.meta.copy()
             bands = np.empty(
                 (nmb_bands, out_meta['height'], out_meta['width']))
-        bands[i] = band_tif.read(1)
-        print(f'{np.min(bands[i]) = }')
-        print(f'{np.max(bands[i]) = }')
+        bands[i] = band.read(1)
+        band.close()
 
 bands = adjust_values(bands, 5000, 10000)
 
 # Create directory for output image
-out_dir = f'{args.image_dir}/out'
+out_dir = f'{args.band_dir}/out'
 try:
     os.mkdir(out_dir)
 except FileExistsError:
